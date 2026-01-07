732e2f5c229824270a5192b586cc00e861295e1d
Manojkumar S <manojkumar.smk09@gmail.com>
2026-01-07 10:16:55 +0000

    ui5 binding

--- ui5.databinding/webapp/controller/App.controller.js
+++ ui5.databinding/webapp/controller/App.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/m/library",
-	"sap/ui/core/mvc/Controller"
-], (mobileLibrary, Controller) => {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/type/Currency"
+], (mobileLibrary, Controller, Currency) => {
 	"use strict";
 
 	return Controller.extend("ui5.databinding.controller.App", {
@@ -12,6 +13,18 @@ sap.ui.define([
 				`${sFirstName}.${sLastName}@example.com`,
 				oBundle.getText("mailSubject", [sFirstName]),
 				oBundle.getText("mailBody"));
+		},
+		formatStockValue(fUnitPrice, iStockLevel, sCurrCode) {
+			const oCurrency = new Currency();
+
+			return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
+		},
+		onItemSelected(oEvent) {
+			const oSelectedItem = oEvent.getSource();
+			const oContext = oSelectedItem.getBindingContext("products");
+			const sPath = oContext.getPath();
+			const oProductDetailPanel = this.byId("productDetailsPanel");
+			oProductDetailPanel.bindElement({ path: sPath, model: "products" });
 		}
 	});
 });
